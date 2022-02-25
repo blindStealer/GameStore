b9b0b369c3195c793ee1fc502ffee248009325ba
slava <slava.petrovskij@mail.ru>
2022-02-25 10:46:05 +0300

    clear react-pattern

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
+
 
 const App = () => {
   return (
-    <BrowserRouter BrowserRouter>
-      <div className="app-wrapper">
-        <HeaderContainer />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Routes>
-            <Route path="/dialogs/*" element={<DialogsContainer />} />
-            <Route path="/profile/:userID" element={<ProfileContainer />} />
-            <Route path="/profile/" element={<ProfileContainer />} />
-            <Route path="/users/*" element={<UsersContainer />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+    <div>
+      123
+    </div>
+    
+  )
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import store from "./redux/redux-store";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
-import { Provider } from 'react-redux'
+
 
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  
+    <App />,
   document.getElementById("root")
 );
 
